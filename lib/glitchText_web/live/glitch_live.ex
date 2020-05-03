20d3b27027a6f06fc50242a4306a305d4a532b12
ismailunal <ismailunal@gmail.com>
2020-05-03 22:42:21 +0100

    random word on live

--- lib/glitchText_web/live/glitch_live.ex
+++ lib/glitchText_web/live/glitch_live.ex
@@ -1,7 +1,33 @@
-defmodule GlitchText.GlitchLive do
+defmodule GlitchTextWeb.GlitchLive do
   use GlitchTextWeb, :live_view
 
   def mount(_params, _session, socket) do
+    if connected?(socket) do
+      :timer.send_interval(Enum.random(200..600), self(), :tick)
+    end
+    socket = assign_word(socket)
+      {:ok, socket}
+  end
 
+  def render(assigns) do
+    ~L"""
+    <div>Happy <%= @new_word %><div>
+    """
+  end
+
+  def handle_info(:tick, socket) do
+    socket = assign_word(socket)
+    {:noreply, socket}
+  end
+
+  defp assign_word(socket) do
+    things = ["coding","knitting","drawing","designing","planning","writing"]
+    junk = "#,@,%,*,&amp;,&lt;,&gt;,_,=,+,[,],|,-,!,?,X"
+
+    random_word = Enum.at(things,Enum.random(0..Enum.count(things)-1))
+
+    assign(socket,
+        new_word: random_word
+      )
   end
 end
