be6024f68cd5969a6b95dfbb8de25bd12c4b1661
ismailunal <ismailunal@gmail.com>
2020-05-04 00:54:10 +0100

    random junk to replace wording introduced

--- lib/glitchText_web/live/glitch_live.ex
+++ lib/glitchText_web/live/glitch_live.ex
@@ -3,7 +3,7 @@ defmodule GlitchTextWeb.GlitchLive do
 
   def mount(_params, _session, socket) do
     if connected?(socket) do
-      :timer.send_interval(Enum.random(200..600), self(), :tick)
+      :timer.send_interval(Enum.random(100..500), self(), :tick)
     end
     socket = assign_word(socket)
       {:ok, socket}
@@ -21,13 +21,14 @@ defmodule GlitchTextWeb.GlitchLive do
   end
 
   defp assign_word(socket) do
-    things = ["coding","knitting","drawing","designing","planning","writing"]
-    junk = "#,@,%,*,&amp;,&lt;,&gt;,_,=,+,[,],|,-,!,?,X"
+    things = ["coding","knitting","drawing","designing","planning","writing","concluding","programming","thinking","painting"]
+    junk = ["#","@","%","*","&amp;","&lt;","&gt;","_","=","+","[","]","|","-","!","?","X"]
 
     random_word = Enum.at(things,Enum.random(0..Enum.count(things)-1))
-
+    pattern = String.slice(random_word,Enum.random(0..String.length(random_word)-1),1)
+    replace_by = Enum.at(junk,Enum.random(0..Enum.count(junk)-1))
     assign(socket,
-        new_word: random_word
+        new_word: String.replace(random_word, pattern, replace_by, global: false)
       )
   end
 end
